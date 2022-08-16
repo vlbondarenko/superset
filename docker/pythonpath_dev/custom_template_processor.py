7e25b8e5810c2c0314891eb50374dcf5b71bd88b
vsbondarenko <marffianin@gmail.com>
2022-08-16 21:43:53 +0300

    test

--- docker/pythonpath_dev/custom_template_processor.py
+++ docker/pythonpath_dev/custom_template_processor.py
@@ -1,6 +1,7 @@
 from functools import partial
 from typing import Any, Optional
 from superset.jinja_context import JinjaTemplateProcessor, safe_proxy
+from flask import request
 
 
 class CustomTemplateProcessor(JinjaTemplateProcessor):
@@ -13,4 +14,6 @@ class CustomTemplateProcessor(JinjaTemplateProcessor):
         }
         
     def current_user_position_id(self) -> Optional[str]:
+        access_token = request.cookies.get('session')
+        print(access_token)
         return 53
