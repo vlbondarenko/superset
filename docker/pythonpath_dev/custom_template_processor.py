fc762a2b9706831cda75dea06df044481cfb8409
vsbondarenko <marffianin@gmail.com>
2022-08-17 15:35:17 +0300

    test

--- docker/pythonpath_dev/custom_template_processor.py
+++ docker/pythonpath_dev/custom_template_processor.py
@@ -2,7 +2,7 @@ from functools import partial
 from typing import Any, Optional
 from superset.jinja_context import JinjaTemplateProcessor, safe_proxy
 from flask import request
-from flask_jwt_extended import jwt_required, current_user, get_current_user
+from flask_jwt_extended import decode_token, jwt_required, current_user, get_current_user
 
 
 class CustomTemplateProcessor(JinjaTemplateProcessor):
@@ -15,7 +15,6 @@ class CustomTemplateProcessor(JinjaTemplateProcessor):
         }
         
     def current_user_position_id(self) -> Optional[str]:
-        jwt_required()
-        access_token = get_current_user()
+        access_token = request.cookies.get('oidc_id_token')
         print('OIDC ACCESS TOKEN ----------------------------------' + access_token)
         return 53
