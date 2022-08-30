e702876ff8c0efb7fc3ec5e849ce4ead514d835e
vsbondarenko <marffianin@gmail.com>
2022-08-30 13:39:58 +0300

    test mapping

--- docker/pythonpath_dev/custom_template_processor.py
+++ docker/pythonpath_dev/custom_template_processor.py
@@ -1,5 +1,6 @@
 from array import array
 from functools import partial
+from traceback import format_exc
 from typing import Any, Iterable, Optional
 from superset.jinja_context import JinjaTemplateProcessor, safe_proxy
 from superset import security_manager
@@ -25,7 +26,8 @@ class CustomTemplateProcessor(JinjaTemplateProcessor):
         oidc = security_manager.oid
         info = oidc.user_getinfo(['regions'])
         regions = info.get('regions')
-        return map(str, regions)
+        for region in regions:
+            yield str(region)
     
     def current_user_segments(self) -> Iterable[int]:
         oidc = security_manager.oid
