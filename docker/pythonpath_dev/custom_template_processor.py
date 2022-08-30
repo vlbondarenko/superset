2490e673a0afbcd7f0a115eb7817bcf0a022afe6
vsbondarenko <marffianin@gmail.com>
2022-08-30 14:26:37 +0300

    fix

--- docker/pythonpath_dev/custom_template_processor.py
+++ docker/pythonpath_dev/custom_template_processor.py
@@ -9,23 +9,24 @@ class CustomTemplateProcessor(JinjaTemplateProcessor):
 
     def set_context(self, **kwargs: Any) -> None:
         super().set_context(**kwargs)
-        self._context[self.engine] = {
-            "current_user_distributor_id": partial(safe_proxy, self.current_user_distributor_id),
-            "current_user_regions" : partial(safe_proxy,self.current_user_regions),
-            "current_user_segments" : partial(safe_proxy,self.current_user_segments)
-        }
-        
+        self._context.update(
+            {
+                "current_user_distributor_id": partial(safe_proxy, self.current_user_distributor_id),
+                "current_user_regions": partial(safe_proxy, self.current_user_regions),
+                "current_user_segments": partial(safe_proxy, self.current_user_segments)
+            })
+
     def current_user_distributor_id(self) -> Optional[str]:
         oidc = security_manager.oid
         info = oidc.user_getinfo(['distributorId'])
         return info.get('distributorId')
-    
+
     def current_user_regions(self):
         oidc = security_manager.oid
         info = oidc.user_getinfo(['regions'])
         regions = info.get('regions')
         return [str(region) for region in regions]
-    
+
     def current_user_segments(self):
         oidc = security_manager.oid
         info = oidc.user_getinfo(['segments'])
